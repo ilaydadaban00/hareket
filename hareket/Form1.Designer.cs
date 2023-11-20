4f260461feeca99a7974f5ef3882515b4505a70c
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-20 12:14:05 +0300

    Add files via upload

---
+++ hareket/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace hareket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.btnHizli = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::hareket.Properties.Resources.angry_birds;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(226, 190);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Location = new System.Drawing.Point(402, 52);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(75, 23);
+            this.btnNormal.TabIndex = 1;
+            this.btnNormal.Text = "normal";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // btnHizli
+            // 
+            this.btnHizli.Location = new System.Drawing.Point(544, 52);
+            this.btnHizli.Name = "btnHizli";
+            this.btnHizli.Size = new System.Drawing.Size(75, 23);
+            this.btnHizli.TabIndex = 2;
+            this.btnHizli.Text = "hızlı";
+            this.btnHizli.UseVisualStyleBackColor = true;
+            this.btnHizli.Click += new System.EventHandler(this.btnHizli_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(402, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "normal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(544, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "normal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnHizli);
+            this.Controls.Add(this.btnNormal);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Button btnHizli;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ hareket/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hareket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormal_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left = pictureBox1.Left + 10;
+        }
+
+        private void btnHizli_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left = pictureBox1.Left + 25;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Right = pictureBox1.Right + 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Right = pictureBox1.Right + 25;
+        }
+    }
+}
